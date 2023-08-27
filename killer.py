348915835eee8f130a17653a7b8722c9f986e70b
Scott King <Lvl4Sword@users.noreply.github.com>
2023-08-26 18:23:18 -0600

    Lots of TODOs..

--- killer.py
+++ killer.py
@@ -11,9 +11,6 @@ import sys
 import time
 from email.mime.text import MIMEText
 import pyudev
-from config import bluetooth_enabled, cipher_choice, debug_enabled, email_destination, email_enabled, email_sender
-from config import email_timeout, log_file, logging_enabled, login_auth, smtp_server, smtp_port, sender_password
-from config import time_format, usb_enabled, user_timezone
 
 VERSION = "0.8.3"
 BT_MAC_REGEX = re.compile(r"(?:[0-9a-fA-F]:?){12}")
@@ -21,6 +18,37 @@ BT_NAME_REGEX = re.compile(r"[0-9A-Za-z ]+(?=\s\()")
 BT_CONNECTED_REGEX = re.compile(r"(Connected: [0-1])")
 usb_devices = {}
 usb_ids = {}
+
+# TODO - This actually needs used.
+#  If someone specifies a config and it's unavailable/mangled..
+#  then we need somewhere to get the config. This is that place.
+default_config = {'ac_file': {'AC': 1},
+                  'battery_file': {'BAT1': 1},
+                  'usb_id_allowlist': {"DEAD:BEEF": 1},
+                  'usb_connected_whitelist': {"DEAD:BEEF": 1},
+                  'cdrom_drive': {"/dev/sr0": 1},
+                  'ethernet_connected_file': {r"/sys/class/net/RUN_DEBUG/carrier": 1},
+                  'bluetooth_paired_allowlist': {"DE:AD:BE:EF:CA:FE": {"name": "Generic Bluetooth Device", "amount": 1},
+                                                 "AB:CD:EF:12:34:56": {"name": "Generic Bluetooth Device 2", "amount": 1}},
+                  'bluetooth_connected_allowlist': {"DE:AD:BE:EF:CA:FE": {"name": "Generic Bluetooth Device", "amount": 1},
+                                                    "AB:CD:EF:12:34:56": {"name": "Generic Bluetooth Device 2", "amount": 1}},
+                  'smtp_server': "mail.example.com",
+                  'smtp_port': 465,
+                  'email_sender': "example@example.com",
+                  'email_destination': ["example@example.com", "example2@example.com"],
+                  'sender_password': "9b§ì]QîiT{¹5¡/ã¦¥µ+På\yµ'¾SéÉA¸^f|BóÚ{PÙóÕm,öÞÇÙú>:ûK65ÏNö¿^Z8bs",
+                  'cipher_choice': "ECDHE-RSA-AES256-GCM-SHA384",
+                  'login_auth': "LOGIN",
+                  'email_timeout': 0.8,
+                  'log_file': "/something/something.txt",
+                  'debug_enabled': True,
+                  'bluetooth_enabled': True,
+                  'email_enabled': True,
+                  'logging_enabled': True,
+                  'usb_enabled': True,
+                  'user_timezone': "US/Eastern",
+                  'time_format': "%Y-%m-%d %I:%M:%S%p"}
+
 
 
 # Root is required for shutting down unless you allow your user to
@@ -257,29 +285,54 @@ def the_args() -> argparse.Namespace:
     # TODO - The different flavors of debug need to actually do what they advertise
     parser.add_argument("-d", "--debug", action="store_true",
                         help="Debug. Prints all info once, without worrying about shutdown.")
-    parser.add_argument("-dni", action="store_true",
-                        help="Debug, non-interactive." +
-                             "Prints all info once, without worrying about shutdown, " +
-                             "AND stores what was detected non-interactively. " +
-                             "Doesn't add any USBs to connected whitelist. " +
-                             "If you need something more fine-grained, use -di for interactive.")
-    parser.add_argument("-di", action="store_true",
+    parser.add_argument("-di", "--debug-interactive", action="store_true",
                         help="Debug, interactive." +
                              "Prints all info once, without worrying about shutdown, " +
                              "AND stores what was detected interactively.")
+    # TODO - This isn't actually paid attention to, change this.
     parser.add_argument("-c", "--config", type=str, default=None,
                         help="Path to a configuration file to use")
+    # TODO - This isn't actually paid attention to, change this.
     parser.add_argument("-lc", "--log-config", type=str, default=None,
                         help="Path to logging configuration file.")
+    parser.add_argument("-e", "--email", action="store_true",
+                        help="Whether e-mail is enabled or not if tampering is detected.")
+    # TODO - Add something that enables/disables local time being set for e-mail/logs
     all_args = parser.parse_args()
     return all_args
+
+
+def verify_custom_config(the_config):
+    if os.path.isfile(args.config):
+        with open(the_config, 'r') as config_file:
+            the_file = config_file.readlines()
+            # TODO - Continue verification of custom config here.
+    else:
+        return None
 
 
 if __name__ == '__main__':
     debug_set = False
     args = the_args()
-    if any([args.debug, args.dni, args.di, debug_enabled]):
+    # TODO - Check if debuginteractive is right
+    if any([args.debug, args.debuginteractive]):
         debug_set = True
+    if args.config:
+        # TODO - actually iterate through all enabled configurations via argparse for this.
+        #  Also verify the file itself to ensure there isn't anything extra, and that the
+        #  format is done in a way that is expected.
+        config = args.config
+        if args.email:
+            from config import cipher_choice, email_destination, email_enabled, email_sender, email_timeout
+            from config import login_auth, smtp_server, smtp_port, sender_password
+        if args.bluetooth:
+            from config import bluetooth_enabled
+    else:
+        # TODO - handle loading the default config file. If that doesn't exist..
+        #  then load defaults from default_config up above.
+        # TODO - These configs need moved somewhere else for their specific items.
+        from config import debug_enabled, time_format, usb_enabled, user_timezone
+        from config import log_file, logging_enabled
     running_as_root = detect_root_user()
     if not running_as_root:
         if debug_set:
@@ -292,8 +345,14 @@ if __name__ == '__main__':
             print("DEBUG: This system is not encrypted with LUKS")
         else:
             sys.exit(1)
+    # TODO - Check if time settings is enabled/disabled. Fall back to UTC if disabled.
+    # Set time for mail
     set_time_settings()
+    # TODO - Check if USB devices are enabled/disabled
+    # Get all current USB devices
     get_all_usb()
+    # TODO - Check if Bluetooth devices are enabled/disabled
+    # Get all current Bluetooth devices
     get_all_bluetooth()
     if not debug_set:
         context = pyudev.Context()
